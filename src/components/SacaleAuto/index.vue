2b51aa3a825bee85e5abc19b058392178555884b
zmx2321 <zmx2321@163.com>
2024-04-12 11:30:29 +0800

    fix: init

--- src/components/SacaleAuto/index.vue
+++ src/components/SacaleAuto/index.vue
@@ -1,20 +1,18 @@
 <template>
     <section class="container">
         <!-- 大屏展示区 -->
-        <div class="scale_box" :style="{
-            width: width + 'px', height: height + 'px',
-        }">
+        <div class="scale_box" :style="{ width: width + 'px', height: height + 'px' }">
             <slot></slot>
         </div>
     </section>
 </template>
 
 <script setup name="ScaleBox">
-import { onMounted, onUnmounted, ref, /* reactive, toRef, */ watch } from "vue";
+import { onMounted, ref, watch, onUnmounted } from "vue";
 import { debounce } from '@/utils/index.js'
+
+/** props 参数 */
 const props = defineProps({
-    /* width: Number,
-    height: Number, */
     width: {
         type: Number,
         default: 1920
@@ -24,30 +22,31 @@ const props = defineProps({
         default: 1080
     },
 });
+/** 公共数据 */
 const width = ref(props.width)
 const height = ref(props.height)
 let scale = ref({
     w: 1,
     h: 1,
 });
-// const width = toRef(props, "width");
-// const height = toRef(props, "height");
-/* let scale = reactive({
-    w: 1,
-    h: 1,
-}); */
-let getScale = () => {
+
+/**
+ * 业务方法
+ */
+const getScale = () => {
     const w = window.innerWidth / width.value;
     const h = window.innerHeight / height.value;
-    let scale = w < h ? w : h;
-    return [scale, scale];
+    let myScale = w < h ? w : h;
+    return [myScale, myScale];
 };
-let setScale = () => {
+const setScale = () => {
     // 缩放比
     scale.value.h = getScale()[0];
     scale.value.w = getScale()[1];
 };
 let reCalc = debounce(setScale);
+
+/** 监听 */
 watch(
     () => width.value,
     () => {
@@ -55,6 +54,7 @@ watch(
     }
 );
 
+/** 生命周期 */
 onMounted(() => {
     setScale();
     window.addEventListener("resize", reCalc);
@@ -66,13 +66,14 @@ onUnmounted(() => {
 
 <style lang="scss" scoped>
 .container {
-    width: 1920px;
-    height: 1080px;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(255, 0, 0, 0.3);
     background-size: cover;
 
     .scale_box {
-        position: absolute;
-        // transform: scale(v-bind('scale.h')) translate(-50%, -50%);
+        position: fixed;
+        transform: scale(v-bind('scale.h')) translate(-50%, -50%);
         transform: scale(v-bind("scale.w"), v-bind("scale.h")) translate(-50%, -50%);
         display: flex;
         flex-direction: column;
@@ -81,7 +82,7 @@ onUnmounted(() => {
         top: 50%;
         transition: 0.2s;
         z-index: 999;
-        background: rgba(255, 0, 0, 0.3);
+        background: #c8f8e3;
     }
 }
 </style>
--- src/views/demo/screen/index.vue
+++ src/views/demo/screen/index.vue
@@ -4,14 +4,16 @@
   </scale-box>
 </template>
 
-<script setup name="screen">
+<script setup>
 import { ref } from "vue";
+
 import ScaleBox from '@/components/SacaleAuto/index.vue'
 
 const scaleConfig = ref({
   pageWidth: 1920,
   pageHight: 1080,
 });
+
 // scaleConfig.value.pageHight = window.screen.availHeight;
 // scaleConfig.value.pageWidth = window.screen.availWidth;
 </script>
