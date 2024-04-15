2c55fce2433b48795cd1a0d1f577935565531034
zmx2321 <zmx2321@163.com>
2024-04-15 15:43:15 +0800

    fix: init

--- src/views/demo/demo4/demo4-1.vue
+++ src/views/demo/demo4/demo4-1.vue
@@ -9,8 +9,10 @@ import { ref, onMounted } from 'vue';
 import * as mapUtils from './mapUtils.js'
 import { Map, View } from 'ol';
 
+import * as olExtent from 'ol/extent';
+
 // import TileLayer from 'ol/layer/Tile';
-import { fromLonLat } from 'ol/proj';
+import { fromLonLat, transform } from 'ol/proj';
 // import { XYZ } from 'ol/source';
 import LinearRing from 'ol/geom/LinearRing';
 import { Point, MultiLineString, LineString, Polygon } from "ol/geom";
@@ -19,63 +21,36 @@ import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
 import { OSM, Vector as VectorSource } from 'ol/source';
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 
-onMounted(() => {
-  // 初始化地图
-  const olMap = mapUtils.initOlMap('olMap')
+// 地图加载完初始化做的一些操作
+const mapInit = (olMap) => {
+  // 移除控件
+  mapUtils.clearControls(olMap)
+}
 
-  // 设置地图
-  setOlmap(olMap)
-})
+// 地图加载完初始化后获取地图的一些信息
+const getMapInitInfo = (olMap) => {
+  // 获取可视区域的左上角和右下角坐标
+  // getCurrentViewPosition(olMap)  // 地图加载时会自动触发一次
+}
 
 // 设置地图
 const setOlmap = (olMap) => {
-  // 获取可视区域的四个坐标
-  getCurrentViewPosition(olMap)
-
-  // console.log(olMap)
-  // addCurve(olMap)
-
-  // setPoint(olMap)
-
-  // mapEvent(olMap)
+  setPoint(olMap)  // 设置标注点
+  addCurve(olMap)  // 设置扇形区
+  mapEvent(olMap)  // 地图事件
 }
 
-// 获取可视区域的四个坐标
+/**
+ * 地图工具方法
+ */
+// 获取可视区域的左上角和右下角坐标
 const getCurrentViewPosition = (olMap) => {
-  console.log(olMap)
+  let viewPosition = mapUtils.getCurrentViewPosition(olMap)
+  console.log("获取可视区域的左上角和右下角坐标", viewPosition)
 }
 
-
-const mapEvent = (olMap) => {
-  const olExtent = ol.extent;
-  olMap.on('moveend', e => {
-
-    console.log(ol)
-    // console.log('zoom', olMap.frameState_.viewState.zoom);
-    // const extent = olMap.frameState_.extent;
-    // console.log('extent', extent);
-    // console.log('TopLeft', olExtent.getTopLeft(extent));
-    // console.log('BottomRight', olExtent.getBottomRight(extent));
-    // console.log(e, olMap)
-    // const extent = olMap.frameState_.extent
-    // console.log('extent', extent)
-    // console.log('topLeft', olMap.extent)
-
-
-    /* var extent = view.calculateExtent(map.value.getSize());
-    var topLeftCoord = getTopLeft(extent);
-    var bottomRightCoord = getBottomRight(extent);
-    var topLeftCoordProj = transform(topLeftCoord, 'EPSG:3857', 'EPSG:4326');
-    var bottomRightCoordProj = transform(bottomRightCoord, 'EPSG:3857', 'EPSG:4326'); */
-
-
-
-
-  })
-
-}
-
-const setPoint = () => {
+// 设置标注点
+const setPoint = (olMap) => {
   // 绘制点、线、面要素
   const pointFeature = new Feature({
     geometry: new Point(fromLonLat([116.410819, 39.916263])),
@@ -105,7 +80,7 @@ const setPoint = () => {
   ) */
 
   // 创建矢量图层
-  const vectorLayer = new VectorLayer({
+  /* const vectorLayer = new VectorLayer({
     source: new VectorSource({
       features: [pointFeature]
     }),
@@ -119,7 +94,7 @@ const setPoint = () => {
     })
   })
 
-  olMap.addLayer(vectorLayer)
+  olMap.addLayer(vectorLayer) */
 
   // 创建矢量图层
   /* const vectorLayer = new VectorLayer({
@@ -130,94 +105,40 @@ const setPoint = () => {
   }); */
 }
 
+// 设置扇形区
+const addCurve = (olMap) => {
+  mapUtils.addCurve(olMap)
+}
+
+// 地图事件
+const mapEvent = (olMap) => {
+  olMap.on('click', e => {
+    console.log("点击获取坐标", transform(e.coordinate, 'EPSG:3857', 'EPSG:4326'))
+
+    mapUtils.removePointTest(olMap)
+    mapUtils.clickSetPointTest(olMap, e)
+
+  })
+
+  olMap.on('moveend', e => {
+    console.log('拖拽移动触发事件', e)
+
+    getCurrentViewPosition(olMap)
+  })
+}
+
+
 /**
-       * APIMethod:OpenLayers绘制扇形的接口扩展
-       * @param origin 圆心
-       * @param radius 半径
-       * @param sides 边数
-       * @param r 弧度
-       * @param angel 旋转角度（扇形右边半径与x正向轴的角度）
-       * @returns {OpenLayers.Geometry.Polygon}
-       */
-const createRegularPolygonCurve = (origin, radius, sides, r, angel) => {
-  let rotation = 360 - r;
-  let angle = Math.PI * ((1 / sides) - (1 / 2));
-  if (rotation) {
-    angle += (rotation / 180) * Math.PI;
-  }
-  let rotatedAngle, x, y;
-  let points = [];
-  for (let i = 0; i < sides; ++i) {
-    let an = i * ((360 - rotation) / 360);
-    rotatedAngle = angle + (an * 2 * Math.PI / sides);
-    x = origin[0] + (radius * Math.cos(rotatedAngle));
-    y = origin[1] + (radius * Math.sin(rotatedAngle));
-    points.push([x, y]);
-  }
-  if (rotation != 0) {
-    points.push(origin);
-  }
-  var ring = new LinearRing(points);
-  ring.rotate(angel / 57.3, origin);
-  let list = ring.getCoordinates()
+ * vue生命周期函数
+ * 挂载后触发
+ */
+onMounted(() => {
+  const olMap = mapUtils.initOlMap('olMap')  // 初始化地图
 
-  return new Polygon([list]);
-}
-
-// 绘制扇形
-const addCurve = () => {
-  let origi_point = fromLonLat([116.410819, 39.916263]);  // 绘制扇形的顶点
-  let circle = createRegularPolygonCurve(origi_point, 500, 100, 30, 90) // 调用绘制扇形的方法得到扇形
-  let feature = new Feature(circle);  // 把扇形加入 feature
-  feature.setStyle(  // 设置一下这个扇形的样式
-    new Style({
-      fill: new Fill({
-        color: 'rgba(32, 157, 230, 0.3)'
-      }),
-      stroke: new Stroke({
-        color: 'rgba(255, 205, 67, 0.3)',
-        width: 2
-      }),
-    })
-  )
-  feature.set('type', 'Curve')  // 这是给这个扇形添加额外的参数 ， 如果是设置id 用 setId方法
-  feature.set('curve', {   // 这是给这个扇形添加额外的参数，这里的id和 setId的id没关系
-    id: 1,
-    title: '测试001',
-    msg: '测试001-1',
-    msg2: '测试001-2',
-  })
-
-  // 创建第二个扇形，和第一个一样
-  let circle1 = createRegularPolygonCurve(origi_point, 500, 100, 30, 45)
-  let feature1 = new Feature(circle1);
-  feature1.setStyle(
-    new Style({
-      fill: new Fill({
-        color: 'rgba(32, 157, 230, 0.3)'
-      }),
-      stroke: new Stroke({
-        color: 'rgba(255, 205, 67, 0.3)',
-        width: 2
-      }),
-    })
-  )
-  feature1.set('type', 'Curve')
-  feature1.set('curve', {
-    id: 2,
-    title: '超级无敌炫酷爆龙战神',
-    msg: '超级无敌炫酷爆龙战神 描述001',
-    msg2: '超级无敌炫酷爆龙战神 描述002',
-  })
-
-  let vectorSource = new VectorSource();  // 创建一个数据源
-  vectorSource.addFeatures([feature, feature1]);   // 把两个扇形加进数据源
-  let vectorLayer = new VectorLayer({     // 创建一个图层，把数据源加进图层
-    source: vectorSource
-  });
-  olMap.addLayer(vectorLayer);   // 把图层加进地图
-}
-
+  mapInit(olMap)  // 地图加载完初始化做的一些操作
+  getMapInitInfo(olMap)  // 地图加载完初始化后获取地图的一些信息
+  setOlmap(olMap)  // 设置地图
+})
 </script>
 
 <style lang="scss">
--- src/views/demo/demo4/mapUtils.js
+++ src/views/demo/demo4/mapUtils.js
@@ -1,11 +1,27 @@
 // map core
 import { Map, View } from 'ol';
-import { fromLonLat } from 'ol/proj';
-import TileLayer from 'ol/layer/Tile';
+// map 加载底图相关
 import { XYZ } from 'ol/source';
+// map 坐标相关
+import { fromLonLat, transform } from 'ol/proj';
+import * as olExtent from 'ol/extent';
+// map 图层相关
+import Feature from 'ol/Feature';
+import { Point, MultiLineString, LineString, Polygon } from "ol/geom";
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';  // VectorLayer表示珊格图层
+import LinearRing from 'ol/geom/LinearRing';
+// map 样式
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
 
 /******************************
- * 地图工具
+ * 变量(非地图)
+ * ****************************
+ */
+let count = 0  // 地图点击打点变量
+
+/******************************
+ * 地图变量(工具)
  * ****************************
  */
 // 地图初始化配置
@@ -29,11 +45,11 @@ const mapInitConfig = {
   ],
   // ol地图基本配置
   view: new View({
-    /* center: fromLonLat([121.63, 29.88]),
+    center: fromLonLat([121.63, 29.88]),
     zoom: 16,
     maxZoom: 17,
-    minZoom: 9 */
-    center: fromLonLat([116.400819, 39.916263]),
+    minZoom: 9,
+    // center: fromLonLat([116.400819, 39.916263]),
     // View默认使用EPSG3857坐标系
     // projection: 'EPSG:4326',
     zoom: 15,
@@ -42,14 +58,179 @@ const mapInitConfig = {
   })
 }
 
-
-/**
+/******************************
  * 地图核心方法供出
+ * ****************************
  */
+// 初始化地图
 export const initOlMap = (target)=> {
   return new Map({
     target,
     layers: mapInitConfig.layers,
     view: mapInitConfig.view
   });
+}
+
+// 获取可视区域的左上角和右下角坐标
+export const getCurrentViewPosition = (olMap) => {
+  const extent = olMap.getView().calculateExtent(olMap.getSize());
+
+  // 获取投影坐标系
+  const topLeftCoord = olExtent.getTopLeft(extent);  // 左上角
+  const bottomRightCoord = olExtent.getBottomRight(extent);  // 右下角
+  // console.log(topLeftCoord, bottomRightCoord)
+
+  // 将投影坐标转换为地理坐标
+  const topLeft = transform(topLeftCoord, 'EPSG:3857', 'EPSG:4326');
+  const bottomRight = transform(bottomRightCoord, 'EPSG:3857', 'EPSG:4326');
+  // console.log(topLeft, bottomRight)
+
+  return {
+    topLeft,
+    bottomRight,
+  }
+
+}
+
+// 移除所有控件
+export const clearControls = (olMap) => {
+  olMap.getControls().clear()
+
+  // 重新添加一个默认的缩放控件（如果需要）
+  // olMap.addControl(new control.ZoomSlider());
+}
+
+
+/******************************
+ * 测试
+ * ****************************
+ */
+// 移除标注
+export const removePointTest = (olMap)=> {
+  const layers = olMap.getLayers();
+  layers.forEach(item => {
+    if (item instanceof VectorLayer) olMap.removeLayer(item);
+  });
+}
+
+// 点击打点
+export const clickSetPointTest = (olMap, e)=> {
+  const features = [];
+  // console.log(e.coordinate); // 获取坐标
+
+  const iconFeature = new Feature({
+    geometry: new Point(e.coordinate),
+    name: count++,
+    location: e.coordinate
+  });
+  const style = new Style({
+    image: new CircleStyle({
+      radius: 10,
+      fill: new Fill({
+        color: '#f49d41'
+      }),
+      stroke: new Stroke({
+        color: '#836365',
+        width: 1
+      })
+    })
+  });
+  iconFeature.setStyle(style);
+  features.push(iconFeature);
+  const vectorSource = new VectorSource({
+    features
+  });
+  const vectorLayer = new VectorLayer({
+    source: vectorSource,
+    opacity: 0.8
+  });
+  olMap.addLayer(vectorLayer);
+}
+
+/**
+       * APIMethod:OpenLayers绘制扇形的接口扩展
+       * @param origin 圆心
+       * @param radius 半径
+       * @param sides 边数
+       * @param r 弧度
+       * @param angel 旋转角度（扇形右边半径与x正向轴的角度）
+       * @returns {OpenLayers.Geometry.Polygon}
+       */
+const createRegularPolygonCurve = (origin, radius, sides, r, angel) => {
+  let rotation = 360 - r;
+  let angle = Math.PI * ((1 / sides) - (1 / 2));
+  if (rotation) {
+    angle += (rotation / 180) * Math.PI;
+  }
+  let rotatedAngle, x, y;
+  let points = [];
+  for (let i = 0; i < sides; ++i) {
+    let an = i * ((360 - rotation) / 360);
+    rotatedAngle = angle + (an * 2 * Math.PI / sides);
+    x = origin[0] + (radius * Math.cos(rotatedAngle));
+    y = origin[1] + (radius * Math.sin(rotatedAngle));
+    points.push([x, y]);
+  }
+  if (rotation != 0) {
+    points.push(origin);
+  }
+  var ring = new LinearRing(points);
+  ring.rotate(angel / 57.3, origin);
+  let list = ring.getCoordinates()
+
+  return new Polygon([list]);
+}
+
+// 绘制扇形
+export const addCurve = (olMap) => {
+  let origi_point = fromLonLat([121.63, 29.88]);  // 绘制扇形的顶点
+  let circle = createRegularPolygonCurve(origi_point, 500, 100, 30, 90) // 调用绘制扇形的方法得到扇形
+  let feature = new Feature(circle);  // 把扇形加入 feature
+  feature.setStyle(  // 设置一下这个扇形的样式
+    new Style({
+      fill: new Fill({
+        color: 'rgba(32, 157, 230, 0.3)'
+      }),
+      stroke: new Stroke({
+        color: 'rgba(255, 205, 67, 0.3)',
+        width: 2
+      }),
+    })
+  )
+  feature.set('type', 'Curve')  // 这是给这个扇形添加额外的参数 ， 如果是设置id 用 setId方法
+  feature.set('curve', {   // 这是给这个扇形添加额外的参数，这里的id和 setId的id没关系
+    id: 1,
+    title: '测试001',
+    msg: '测试001-1',
+    msg2: '测试001-2',
+  })
+
+  // 创建第二个扇形，和第一个一样
+  let circle1 = createRegularPolygonCurve(origi_point, 500, 100, 30, 45)
+  let feature1 = new Feature(circle1);
+  feature1.setStyle(
+    new Style({
+      fill: new Fill({
+        color: 'rgba(32, 157, 230, 0.3)'
+      }),
+      stroke: new Stroke({
+        color: 'rgba(255, 205, 67, 0.3)',
+        width: 2
+      }),
+    })
+  )
+  feature1.set('type', 'Curve')
+  feature1.set('curve', {
+    id: 2,
+    title: '超级无敌炫酷爆龙战神',
+    msg: '超级无敌炫酷爆龙战神 描述001',
+    msg2: '超级无敌炫酷爆龙战神 描述002',
+  })
+
+  let vectorSource = new VectorSource();  // 创建一个数据源
+  vectorSource.addFeatures([feature, feature1]);   // 把两个扇形加进数据源
+  let vectorLayer = new VectorLayer({     // 创建一个图层，把数据源加进图层
+    source: vectorSource
+  });
+  olMap.addLayer(vectorLayer);   // 把图层加进地图
 }
